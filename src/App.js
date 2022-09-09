027d182b2bc2659feaaf9b3be11a5042fca70084
Bjarne Voigtländer <bjarne.voigtlaender@icloud.com>
2022-09-09 19:36:10 +0200

    - basic tailwind config - static login page - some components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-function App() {
+import Button from "./components/Button";
+import Headline from "./components/Headline";
+import Input from "./components/Input";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="max-w-sm flex flex-col items-center justify-center">
+        <Headline partial={'register'}></Headline>
+        <div className="flex flex-col gap-4">
+          <Input type="text" placeholder="username" />
+          <Input type="email" placeholder="email" />
+          <div className="flex w-full gap-4">
+            <Button>continue</Button><Button>already registered?</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@
 module.exports = {
   content: [
     "./src/**/*.js",
+    "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          light: '#fff',
+          dark: '#242437'
+        },
+        waterloo: {
+          100: '#79799b',
+          900: '#242437'
+        },
+        input: '#d9d9d9'
+      }
+    },
   },
   plugins: [],
 }
