235257965823f4bd0987cd6baecc278e4332ea7e
Bjarne Voigtländer <bjarne.voigtlaender@icloud.com>
2022-09-16 21:59:41 +0200

    what about using states :thinking:

--- src/components/ProfileBar.js
+++ src/components/ProfileBar.js
@@ -5,27 +5,28 @@ import profPicSample from '../assets/images/profpic-sample.png'
 export default function ProfileBar() {
 
   const [profPic, setProfPic] = useState(undefined);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => setProfPic(profPicSample), []);
 
   const onMouseEnterSearchIcon = () => {
-    document.querySelector('#pb-search').className = 'flex items-center bg-[#414163] text-[12px] rounded-lg w-52 duration-1000 focus:outline-0';
+    setIsOpen(true);
   }
 
   const onBlurSearchIcon = () => {
-    document.querySelector('#pb-search').className = 'flex w-0 duration-500';
+    setIsOpen(false);
   }
 
   return (
     <div className="profile-bar flex h-full px-4 items-center justify-between text-white border-r-0 border-white">
-      <img alt="profile pic" src={profPic} className="w-12"/>
-      <div 
-        className="flex rounded-lg bg-[#414163]" 
+      <img alt="profile pic" src={profPic} className="w-12" />
+      <div
+        className="flex rounded-lg bg-[#414163]"
         onMouseEnter={onMouseEnterSearchIcon}
         onBlur={onBlurSearchIcon}
       >
         <img className="w-8 h-8" alt="search" src={searchIcon} />
-        <input id="pb-search" type="text" className="flex w-0" placeholder="Search..."/>
+        <input id="pb-search" type="text" className={`flex focus:outline-0 duration-500 items-center bg-[#414163] text-[12px] rounded-lg ${isOpen ? 'w-52' : 'w-0'}`} placeholder="Search..." />
       </div>
     </div>
   );
