7e9413fc4dbd84ace568d10f794683291b535434
David Kurniadi Weinardy <david.kurniadi.weinardy@outlook.com>
2022-09-17 17:33:09 +0200

    Auto focus 📸 on search input after expanding

--- src/components/ProfileBar.js
+++ src/components/ProfileBar.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import searchIcon from '../assets/icons/search.png';
 import profPicSample from '../assets/images/profpic-sample.png'
 
 export default function ProfileBar() {
 
-  const [profPic, setProfPic] = useState(undefined);
-  const [isOpen, setIsOpen] = useState(false);
+  const [profPic, setProfPic]     = useState(undefined);
+  const [isOpen, setIsOpen]       = useState(false);
+
+  const searchRef                 = useRef(null);
 
   useEffect(() => setProfPic(profPicSample), []);
 
   const onMouseEnterSearchIcon = () => {
     setIsOpen(true);
+    searchRef.current.focus();
   }
 
   const onBlurSearchIcon = () => {
@@ -26,7 +29,13 @@ export default function ProfileBar() {
         onBlur={onBlurSearchIcon}
       >
         <img className="w-8 h-8" alt="search" src={searchIcon} />
-        <input id="pb-search" type="text" className={`flex focus:outline-0 duration-500 items-center bg-[#414163] text-[12px] rounded-lg ${isOpen ? 'w-52' : 'w-0'}`} placeholder="Search..." />
+        <input 
+          id="pb-search" 
+          type="text" 
+          ref={searchRef} 
+          className={`flex focus:outline-0 duration-500 items-center bg-[#414163] text-[12px] rounded-lg ${isOpen ? 'w-52' : 'w-0'}`} 
+          placeholder="Search..." 
+        />
       </div>
     </div>
   );
