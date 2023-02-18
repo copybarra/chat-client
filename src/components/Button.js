11f20cd5ba8fdf19b4b05deedc57c8c98faf7d37
Bjarne Voigtländer <bjarne.voigtlaender@icloud.com>
2023-02-18 13:34:42 +0100

    create the gruene wiese

--- src/components/Button.js
+++
@@ -1,3 +0,0 @@
-export default function Button({ children, ...props }) {
-  return <button className="bg-input hover:bg-opacity-80 rounded-full p-4" {...props}>{children}</button>
-}
--- src/components/ChatMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ChatMessage({ id, content, own, server }) {
-  return (
-    <div key={id}className={`flex flex-col flex-wrap w-fit h-fit max-w-[60%] basis-auto bg-[#414163] font-light rounded-2xl p-2 ${server ? 'self-center' : own ? 'self-end' : 'self-start'}`}>
-      <p className="px-2 text-sm">{content}</p>
-    </div>
-  )
-}
